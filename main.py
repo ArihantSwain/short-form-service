2e3fd9ac10cc2164c5396964e2eb1e7789db290e
ArihantSwain <arihantswain06@gmail.com>
2024-06-04 13:54:01 +0900

    deconstructing comment thread API, wip

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import googleapiclient.errors
 # create YOUTUBE API call service
 api_service_name = "youtube"
 api_service_version = "v3"
-DEVELOPER_KEY = "xxx"
+DEVELOPER_KEY = "AIzaSyDZplUSKPY_WyGCoYShjCHAykfA6-EBQtY" # ENTER API CREDENTIALS KEY HERE
 youtube = googleapiclient.discovery.build(api_service_name, api_service_version, developerKey = DEVELOPER_KEY)
 
 # initially prompt for a url -- need to create a URL verifier
@@ -15,6 +15,19 @@ number_of_clips = input("Enter a number of clips to create: ")
 video_id = video_url[-11:]
 
 # comb through comments and make a list of comment ids based on number of appearances
-request = youtube.commentThreads().list(part="snippet", videoId=video_id, maxResults=100)
+request = youtube.commentThreads().list(part="snippet,replies", videoId=video_id, maxResults=100)
 response = request.execute()
-print(response)
+valid = response
+commentlist = []
+
+print(valid.items())
+
+for x in range(4):
+    valid.pop(valid.items().get(x))
+
+print(valid)
+count = 0
+for x in range(5):
+    print(valid.get(x))
+    if x[0] != "items":
+        valid.pop(x)
