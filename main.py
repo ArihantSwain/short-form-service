be2d9fc0f8cdd1208be8f8805514fb4a414e1383
ArihantSwain <arihantswain06@gmail.com>
2024-06-29 07:31:39 -0600

    denested the dictionaries provided by Youtube API to retrieve a list of timestamps

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import googleapiclient.discovery
 import googleapiclient.errors
 
+
+# has letters is a method to check if a str has letters
+def has_letters(entered_string):
+    for x in entered_string:
+        if x.isalpha():
+            return True
+    return False
+
+
 # create YOUTUBE API call service
 api_service_name = "youtube"
 api_service_version = "v3"
-DEVELOPER_KEY = "" # ENTER API CREDENTIALS KEY HERE
-youtube = googleapiclient.discovery.build(api_service_name, api_service_version, developerKey = DEVELOPER_KEY)
+DEVELOPER_KEY = ""  # ENTER API CREDENTIALS KEY HERE
+youtube = googleapiclient.discovery.build(api_service_name, api_service_version, developerKey=DEVELOPER_KEY)
 
 # initially prompt for a url -- need to create a URL verifier
 video_url = input("Enter a video url: ")
@@ -18,16 +27,38 @@ video_id = video_url[-11:]
 request = youtube.commentThreads().list(part="snippet,replies", videoId=video_id, maxResults=100)
 response = request.execute()
 valid = response
-commentlist = []
+int_response_list = valid.get('items')
+comment_outer = []
+comment_secondary = []
+comment_tertiary = []
+comment_final = []
+timestamps = []
+timestamps_culled = []
 
-print(valid.items())
+# the response given is a dictionary of lists of dictionaries, which when keyed, also leads to another set
+# of dictionaries. these dictionaries are nested heavily, the code below retrieves comments from this nested
+# format, thus allowing us to gather the necessary timestamps.
+for i in int_response_list:
+    comment_outer.append(i.get('snippet'))
 
-for x in range(4):
-    valid.pop(valid.items().get(x))
+for i in comment_outer:
+    comment_secondary.append(i.get('topLevelComment'))
 
-print(valid)
-count = 0
-for x in range(5):
-    print(valid.get(x))
-    if x[0] != "items":
-        valid.pop(x)
+for i in comment_secondary:
+    comment_tertiary.append(i.get('snippet'))
+
+for i in comment_tertiary:
+    comment_final.append(i.get('textDisplay'))
+
+for i in comment_final:
+    if ">" in i:
+        print(i)
+        new_string = i.split('>', 1)
+        final_string = new_string[1].split('<', 1)
+        timestamps.append(final_string[0])
+
+for i in timestamps:
+    if i != "" and has_letters(i) is False:
+        timestamps_culled.append(i)
+
+print(timestamps_culled)
